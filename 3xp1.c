c240cc5c7425c3270f42dc57316a64bdb30ee14e
Sarvesh MD <76893756+SarveshMD@users.noreply.github.com>
2021-08-19 21:32:03 +0530

    Added noPrintf.c and made 3xp1.c more efficient Used integers wherever possible to speed up

--- 3xp1.c
+++ 3xp1.c
@@ -1,37 +1,36 @@
 #include <stdio.h>
-#include <math.h>
 
 int main(void) {
 	long int num, n;
-	// printf("Enter a number: \n");
-	// scanf("%i", &num);
-	num = 10;
+	printf("Enter a number: \n");
+	scanf("%ld", &num);
+	printf("\n\n");
 	n = 0;
 	while (n < num) {
 		n++;
 		int temp;
 		long int nums[1000000], i;
-		i = n+0.0;
-		temp = 0.0;
-		while (i!=1.0) {
-			if (fmod(i,2.0)==0.0) {
-				i= i/2.0;
+		i = n+0;
+		temp = 0;
+		while (i!=1) {
+			if (i%2==0) {
+				i= i/2;
 			}
-			else if (fmod(i,2.0)==1.0) {
-				i = (3.0*i)+1.0;
+			else if (i%2==1.0) {
+				i = (3*i)+1;
 			}
-			nums[temp] = i+0.0;
+			nums[temp] = i+0;
 			temp++;
 		}
-		printf("%.0ld: %i\n",n,temp);
+		printf("%ld: %i\n",n,temp);
 		printf("[");
 		int reached = 0;
 		for (int x=0;x<temp;x++) {
-			if (nums[x] == 4.0 || nums[x] == 2.0 || nums[x] == 1.0) {
+			if (nums[x] == 4 || nums[x] == 2 || nums[x] == 1) {
 				reached = 1;
 			}
 			if (reached) {
-				printf("%.0ld, ",nums[x]);
+				printf("%ld, ",nums[x]);
 			}
 		}
 		printf("\b\b]\n\n");
---
+++ noPrint.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+	long int num, n;
+	num = 1000000;
+	n = 0;
+	while (n < num) {
+		n++;
+		int temp;
+		long int nums[1000000], i;
+		i = n+0;
+		temp = 0;
+		while (i!=1) {
+			if (i%2==0) {
+				i= i/2;
+			}
+			else if (i%2==1.0) {
+				i = (3*i)+1;
+			}
+			nums[temp] = i+0;
+			temp++;
+		}
+	}
+	printf("Program exited successfully....\n");
+}
+
