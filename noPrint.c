f3df5a0caa4220eded96865b81b95d8ac4ad2d4d
Sarvesh MD <76893756+SarveshMD@users.noreply.github.com>
2021-08-20 15:00:41 +0530

    Works from the number itself

--- noPrint.c
+++ noPrint.c
@@ -10,6 +10,7 @@ int main(void) {
 		long int i, temp;
 		i = n+0;
 		temp = 0;
+		arr[n-1][temp+1] = i+0;
 		// printf("\n\nn: %ld\n", n);
 		while (i!=1) {
 			if (i%2==0) {
@@ -20,14 +21,14 @@ int main(void) {
 			}
 			temp++;
 			//printf("i: %ld\n", i);
-			arr[n-1][temp] = i+0;
+			arr[n-1][temp+1] = i+0;
 		}
-		arr[n-1][0] = temp;
+		arr[n-1][0] = temp+1;
 	}
 	printf("Program exited successfully....\n");
 	for(int l1 = 0;l1<num;l1++) {
 		//printf("l1: %d\n", l1);
-		printf("\narr[%d][0] = %ld\nNUM: %d\n", l1, arr[l1][0], l1+1);
+		printf("\narr[%d][0] = %ld\n", l1, arr[l1][0]);
 		for(int l2 = 0; l2<arr[l1][0]; l2++) {
 			//printf("l2: %d\n", l2);
 			printf("arr[%d][%d] = %ld\n", l1, l2+1, arr[l1][l2+1]);
