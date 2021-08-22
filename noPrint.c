914e16a7e4e4ae6eca0b75f4731913db0eea15ae
Sarvesh MD <76893756+SarveshMD@users.noreply.github.com>
2021-08-22 15:10:37 +0530

    Perfect and efficient print after calculate

--- noPrint.c
+++ noPrint.c
@@ -4,7 +4,8 @@ int main(void) {
 	long int num, n;
 	printf("Enter a number: \n");
 	scanf("%ld", &num);
-long int arr[num+0][10000]; // This line needs a lot of memory, so blows up.
+	printf("\n");
+	long int arr[num+0][5]; // This line needs a lot of memory, so blows up.
 	n = 0;
 	printf("Crunching the numbers...\n");
 	while (n < num) {
@@ -12,8 +13,18 @@ long int arr[num+0][10000]; // This line needs a lot of memory, so blows up.
 		long int i, temp;
 		i = n+0;
 		temp = 0;
-		arr[n-1][temp+1] = i+0;
-		while (i!=1) {
+		int ready = 0;
+		int prev = i+0;
+		int noar;
+		while (prev!=1) {
+			if ((!ready) && (i<=4) && (i!=3)) {
+				ready = 1;
+				noar = 1;
+			}
+			if (ready) {
+				arr[n-1][(++noar)] = i+0;
+			}
+			prev = i+0;
 			if (i%2==0) {
 				i= i/2;
 			}
@@ -21,22 +32,20 @@ long int arr[num+0][10000]; // This line needs a lot of memory, so blows up.
 				i = (3*i)+1;
 			}
 			temp++;
-			arr[n-1][temp+1] = i+0;
 		}
-		arr[n-1][0] = temp+1;
+		if(i==1) {
+			temp = 1;
+			noar = 2;
+			arr[n-1][2] = 1;
+		}
+		arr[n-1][0] = temp-1;
+		arr[n-1][1] = noar-1;
 	}
 	printf("Calculation Completed Successfully...\n");
 	for(int l1 = 0;l1<num;l1++) {
-		printf("\n%ld: %ld\n[", arr[l1][1], arr[l1][0]);
-		int l2;
-		if(arr[l1][0]>=3) {
-			l2 = arr[l1][0]-3;
-		}
-		else {
-			l2 = 0;
-		}
-		for(; l2<arr[l1][0]; l2++) {
-			printf("%ld, ", arr[l1][l2+1]);
+		printf("\n%d: %ld\n[", l1+1, arr[l1][0]);
+		for(int l2=0;l2<arr[l1][1];l2++) {
+			printf("%ld, ", arr[l1][l2+2]);
 		}
 		printf("\b\b]\n");
 	}
